ac39130049376332b08b5fbb696fa483358046c4
Julian Raschke <julian@raschke.de>
2015-07-20 19:27:46 +0200

    Use the new Gosu::useResourceDirectory()

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,9 @@
 #include "Game.h"
 #include <Gosu/Gosu.hpp>
 
-#ifdef GOSU_IS_UNIX
-#include <unistd.h>
-#endif
-
 int main()
 {
-#ifdef GOSU_IS_UNIX
-    // If Gosu suggests loading resources from an absolute path (instead of '.'
-    // or even ''), then chdir() into that path so that loading things from
-    // media/foo.bar automatically works.
-    std::wstring resourcePath = Gosu::resourcePrefix();
-    if (resourcePath.front() == '/') {
-        chdir(Gosu::wstringToUTF8(resourcePath).c_str());
-    }
-#endif
+    Gosu::useResourceDirectory();
     
     Game window;
     window.show();
